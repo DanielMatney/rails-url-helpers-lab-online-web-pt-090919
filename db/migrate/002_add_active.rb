a7bf9e34505c6db7e8fde1f007f6aac09f2a78be
Daniel Matney <danieljmatney@gmail.com>
2020-02-08 13:22:02 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_active.rb
+++ db/migrate/002_add_active.rb
@@ -1,5 +1,5 @@
 class AddActive < ActiveRecord::Migration[5.0]
   def change
-    add_column :active, :active, :boolean, default: false
+    add_column :students, :active, :boolean, default: false
   end
 end
