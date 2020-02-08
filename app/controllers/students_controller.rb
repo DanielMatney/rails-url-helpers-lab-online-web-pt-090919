73739a49b1b915ab593b23d7bac61bd90317739f
Daniel Matney <danieljmatney@gmail.com>
2020-02-08 13:56:47 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   
   def activate
     @student = Student.find(params[:id])
-    if @student.active == true
+    if @student.active
       @student.active = false
     else
       @student.active = true
